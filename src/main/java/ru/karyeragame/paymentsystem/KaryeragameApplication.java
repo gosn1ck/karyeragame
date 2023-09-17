3a8b4bce5f32943ed59491bacd5eee80696872b4
Николай Верховцев <gosn1ck@yandex.ru>
2023-09-17 20:42:04 +0300

    feat: init empty project

---
+++ src/main/java/ru/karyeragame/paymentsystem/KaryeragameApplication.java
@@ -0,0 +1,13 @@
+package ru.karyeragame.paymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaryeragameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaryeragameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/karyeragame/paymentsystem/KaryeragameApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.karyeragame.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaryeragameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
