f8700df262ca955e031c1e733697161cb436a8d2
Andrey <molodcov.ao@yandex.ru>
2023-10-04 00:43:25 +0400

    Времянка

--- src/main/java/ru/karyeragame/paymentsystem/mailsender/controller/EmailController.java
+++ src/main/java/ru/karyeragame/paymentsystem/mailsender/controller/EmailController.java
@@ -2,9 +2,11 @@ package ru.karyeragame.paymentsystem.mailsender.controller;
 
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.karyeragame.paymentsystem.mailsender.resource.EmailMessage;
 import ru.karyeragame.paymentsystem.mailsender.service.EmailService;
@@ -20,15 +22,17 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping("/send-email")
-    public ResponseEntity sendEmail(@RequestBody EmailMessage emailMessage) {
+    @ResponseStatus(HttpStatus.OK)
+    public String sendEmail(@RequestBody EmailMessage emailMessage) {
         emailService.sendSimpleMessage(emailMessage.to(), emailMessage.subject(), emailMessage.message());
-        return ResponseEntity.ok("Success");
+        return "Success";
     }
 
     @PostMapping("/send-email/attach")
-    public ResponseEntity sendEmailWithAttach(@RequestBody EmailMessage emailMessage) throws MessagingException {
+    @ResponseStatus(HttpStatus.OK)
+    public String sendEmailWithAttach(@RequestBody EmailMessage emailMessage) throws MessagingException {
         emailService.sendMessageWithAttachment(emailMessage.to(), emailMessage.subject(),
                 emailMessage.message(), emailMessage.path());
-        return ResponseEntity.ok("Success");
+        return "Success";
     }
 }
