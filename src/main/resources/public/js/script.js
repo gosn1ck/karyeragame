7ad88cc958b2775f5edb8d6d026b8fdca704c302
Asemi1 <bella33355@gmail.com>
2023-10-08 23:35:46 +0300

    Создана страница регистрации

---
+++ src/main/resources/public/js/script.js
@@ -0,0 +1,22 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+// var z = document.getElementById("btn");
+
+function register() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  // x.style.left = "-400px";
+  // y.style.left = "50px";
+  // z.style.left = "110px";
+  console.log("reg")
+}
+
+function login() {
+  x.style.left = "50px";
+  y.style.left = "450px";
+  // x.style.left = "50px";
+  // y.style.left = "450px";
+  // z.style.left = "0px";
+  console.log("log")
+
+}
