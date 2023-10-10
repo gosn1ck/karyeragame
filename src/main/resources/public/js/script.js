4ba811bd0e3333b28a48f7e00271c22d74a2d2c5
Asemi1 <bella33355@gmail.com>
2023-10-10 13:34:14 +0300

    Доработка формы

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -1,22 +1,24 @@
 var x = document.getElementById("login");
 var y = document.getElementById("register");
-// var z = document.getElementById("btn");
+// var bx = document.getElementById("button-login");
+// var by = document.getElementById("button-register");
 
 function register() {
-  x.style.left = "-400px";
-  y.style.left = "50px";
-  // x.style.left = "-400px";
-  // y.style.left = "50px";
-  // z.style.left = "110px";
+  x.style.left = "-100%";
+  y.style.left = "5%";
+
+  document.querySelector("#button-login").classList.remove("buttons__form-type--active");
+  document.querySelector("#button-register").classList.add("buttons__form-type--active");
   console.log("reg")
 }
 
 function login() {
-  x.style.left = "50px";
-  y.style.left = "450px";
-  // x.style.left = "50px";
-  // y.style.left = "450px";
-  // z.style.left = "0px";
+  x.style.left = "5%";
+  y.style.left = "100%";
+
+  document.querySelector("#button-login").classList.add("buttons__form-type--active");
+  document.querySelector("#button-register").classList.remove("buttons__form-type--active");
+
   console.log("log")
 
 }
