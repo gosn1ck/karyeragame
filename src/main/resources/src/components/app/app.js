fcb59337e0d0f87982962094f1631f29cb235580
Asemi1 <bella33355@gmail.com>
2023-11-12 13:34:26 +0300

    Добавление и первичная настройка axios

--- src/main/resources/src/components/app/app.js
+++ src/main/resources/src/components/app/app.js
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="profile" element={<Profile />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     // <div className="app">
